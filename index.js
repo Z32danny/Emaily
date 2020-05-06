ba1a4b926f7518fa438c44808417af7c746f89f0
Daniel Ruiz <danielruiz@daniels-mbp.home>
2020-05-06 14:40:56 -0700

    configured mongoDB and mongoose

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // import express
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 // require passport.js file
 require('./services/passport');
+require('./models/User');
+
+mongoose.connect('mongodb+srv://danielruizgc8:Fj4BlhsVUccBLEVc@emaily-saatm.mongodb.net/test?retryWrites=true&w=majority');
 
 // initiate express app instance
-const app = express();
+const app = express(keys.mongoURI);
 
 // little trick: require statement(that returns a function), then immediately call that function with the app object
 require('./routes/authRoutes')(app);
